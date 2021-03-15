4c7444fd6592ed414bab0ebd6dcf75da80c37410
DavoodKhoshnood <khoshnood1977@gmail.com>
2021-03-15 11:14:02 +0000

    First web file

--- app.js
+++ app.js
@@ -3,9 +3,18 @@
   angular.module('myFirstApp',[])
 
   .controller('MyFirstController',function ($scope) {
-$scope.name =  "Adam";
-$scope.sayHello = function () {
-  return "Hello Coursera!";
+$scope.name =  "";
+$scope.totalValue = 0;
+$scope.displayValue = function () {
+  var totalNameValue = calcValue($scope.name);
+  $scope.totalValue = totalNameValue;
+}
+function calcValue(string) {
+  var newValue = 0;
+for (var i = 0; i < string.length; i++) {
+  newValue += string.charCodeAt(i);
+}
+return newValue;
 }
   });
 })();
